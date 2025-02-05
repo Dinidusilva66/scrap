cdc62ae1b9a00feb8a7bcf031c14c85958e60dae
Dinidusilva66 <118092626+Dinidusilva66@users.noreply.github.com>
2025-02-06 01:43:55 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "cineru-api",
+  "version": "1.0.0",
+  "description": "A simple API to scrape Cineru.lk search results.",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "axios": "^0.27.2",
+    "cheerio": "^1.0.0-rc.12",
+    "express": "^4.17.1"
+  }
+}

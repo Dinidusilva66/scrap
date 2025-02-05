a298fb5bad9c00868f91ed91b0ec4301e0481069
Dinidusilva66 <118092626+Dinidusilva66@users.noreply.github.com>
2025-02-06 02:54:05 +0530

    Update package.js

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
   "version": "1.0.0",
   "main": "index.js",
   "dependencies": {
-    "express": "^4.17.1",      // Add express dependency here
+    "express": "^4.17.1",      // Add express dependency
     "axios": "^0.27.2",
     "cheerio": "^1.0.0-rc.12"
   },
